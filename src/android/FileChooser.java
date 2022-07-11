e00193844409c9979e4de906a15e4fdb98010719
kaveepart <35480359+kaveepart@users.noreply.github.com>
2022-07-11 17:36:51 +0700

    Support android 11 onwards.

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -54,6 +54,12 @@ public class FileChooser extends CordovaPlugin {
     }
 
     @Override
+    public void onDestroy() {
+        clearCache(cordova.getContext());
+        super.onDestroy();
+    }
+    
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (requestCode == PICK_FILE_REQUEST && callback != null) {
@@ -65,7 +71,13 @@ public class FileChooser extends CordovaPlugin {
                 if (uri != null) {
 
                     Log.w(TAG, uri.toString());
-                    callback.success(uri.toString());
+                    
+                    String path = uri.toString();
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        path = getPath(cordova.getContext(), uri);
+                    }
+                    callback.success(path);
 
                 } else {
 
@@ -82,5 +94,96 @@ public class FileChooser extends CordovaPlugin {
                 callback.error(resultCode);
             }
         }
+    }
+    
+    public static boolean clearCache(final Context context) {
+        try {
+            final File cacheDir = new File(context.getCacheDir() + "/file_picker/");
+            final File[] files = cacheDir.listFiles();
+
+            if (files != null) {
+                for (final File file : files) {
+                    file.delete();
+                }
+            }
+        } catch (final Exception ex) {
+            Log.e(TAG, "There was an error while clearing cached files: " + ex.toString());
+            return false;
+        }
+        return true;
+    }
+    
+    public static String getPath(final Context context, final Uri uri) {
+
+        Log.i(TAG, "Caching from URI: " + uri.toString());
+        FileOutputStream fos = null;
+    
+        final String fileName = getFileName(uri, context);
+        final String path = context.getCacheDir().getAbsolutePath() + "/file_picker/" + (fileName != null ? fileName : new Random().nextInt(100000));
+    
+        final File file = new File(path);
+    
+        if(!file.exists()) {
+            file.getParentFile().mkdirs();
+            try {
+                fos = new FileOutputStream(path);
+                try {
+                    final BufferedOutputStream out = new BufferedOutputStream(fos);
+                    final InputStream in = context.getContentResolver().openInputStream(uri);
+                
+                    final byte[] buffer = new byte[8192];
+                    int len = 0;
+                
+                    while ((len = in.read(buffer)) >= 0) {
+                        out.write(buffer, 0, len);
+                    }
+                
+                    out.flush();
+                } finally {
+                    fos.getFD().sync();
+                }
+            } catch (final Exception e) {
+                try {
+                    fos.close();
+                } catch (final IOException | NullPointerException ex) {
+                    Log.e(TAG, "Failed to close file streams: " + e.getMessage(), null);
+                    return null;
+                }
+                Log.e(TAG, "Failed to retrieve path: " + e.getMessage(), null);
+                return null;
+            }
+        }
+    
+        Log.d(TAG, "File loaded and cached at:" + path);
+        return "file://" + path;
+    }
+    
+    public static String getFileName(Uri uri, final Context context) {
+        String result = null;
+    
+        try {
+    
+            if (uri.getScheme().equals("content")) {
+                Cursor cursor = context.getContentResolver().query(uri, new String[]{OpenableColumns.DISPLAY_NAME}, null, null, null);
+                try {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        result = cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME));
+                    }
+                } finally {
+                    cursor.close();
+                }
+            }
+            if (result == null) {
+                result = uri.getPath();
+                int cut = result.lastIndexOf('/');
+                if (cut != -1) {
+                    result = result.substring(cut + 1);
+                }
+            }
+        } catch (Exception ex) {
+            Log.e(TAG, "Failed to handle file name: " + ex.toString());
+        }
+    
+        return result;
     }
 }
