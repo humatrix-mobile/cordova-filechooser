14b6053da80bae5f65441306bcb6028c7d6338a1
kaveepart <35480359+kaveepart@users.noreply.github.com>
2022-07-12 13:46:51 +0700

    Add import file.

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -1,8 +1,12 @@
 package com.megster.cordova;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.OpenableColumns;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
@@ -11,6 +15,13 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 public class FileChooser extends CordovaPlugin {
 
